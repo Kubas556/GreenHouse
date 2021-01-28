5d14394c63db9e312f9b55d9316e7e371e42d197
Kubas556 <jakubsedlak102@gmail.com>
2021-01-28 20:58:52 +0100

    added public folder

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,8 @@
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
+
+firebase.initializeApp({
+  messagingSenderId: "141034843850"
+});
+
+const messaging = firebase.messaging();
